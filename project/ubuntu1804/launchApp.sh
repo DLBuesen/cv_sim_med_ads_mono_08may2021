dcdc955b6a8a1501587c749622da9805b17fc8a1
Darren Buesen <50619551+DLBuesen@users.noreply.github.com>
2021-05-09 17:32:00 +0200

    Update launchApp.sh

--- project/ubuntu1804/launchApp.sh
+++ project/ubuntu1804/launchApp.sh
@@ -12,6 +12,6 @@ gnome-terminal --tab -- bash -c '\
 	docker login;\
 	echo ;\
 	sleep 3;\
-	xhost + && docker run -ti -e DISPLAY=$DISPLAY -p 127.0.0.1:8000:8000 -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTH:/root/.Xauthority -v $(pwd):/home/fenics/shared -v $(pwd):/project -w / dlbuesen/cv_sim_med_ads_mono_08may2021:layer04 ./launchApp.sh'
+	xhost + && docker run -ti -e DISPLAY=$DISPLAY -p 127.0.0.1:8000:8000 -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTH:/root/.Xauthority -v $(pwd):/home/fenics/shared -v $(pwd):/project -w / dlbuesen/cv_sim_med_ads_mono_08may2021:layer05 ./launchApp.sh'
 
 
