0900f02b56e5682b10123b336abdf57622f338ea
DLBuesen <50619551+DLBuesen@users.noreply.github.com>
2021-05-09 14:43:10 +0200

    updated repository

--- project/windows10/docker/install_docker_image.bat
+++ project/windows10/docker/install_docker_image.bat
@@ -1,19 +1,29 @@
 @echo off
 
+echo.
+
+echo Starting script...
+echo.
+
 echo Docker desktop should be running and configured to run Linux containers.
+echo.
+
 echo Should be signed in to dockerhub account.
 
 timeout 3
 
 echo checking login status...
+echo.
 docker login
 
 timeout 3
 
 echo Installing the docker container image...
+echo.
 
 docker pull dlbuesen/cv_sim_med_ads_mono_08may2021:layer04
 
+timeout 3
 echo Listing the installed docker images on the system...
 
 timeout 3
@@ -26,7 +36,9 @@ echo Docker image with ID "8d3c6811a644"
 echo from repository "dlbuesen/cv_sim_med_ads_mono_08may2021"
 echo with tag "layer04"
 echo should appear in the list of images above
+echo.
 
 echo Done
+echo.
 
 pause
