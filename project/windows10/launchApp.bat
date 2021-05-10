6599d55e6a2504fa2dbb4847c29de4eac629e9b5
DLBuesen <50619551+DLBuesen@users.noreply.github.com>
2021-05-10 10:46:55 +0200

    updated repository

--- project/windows10/launchApp.bat
+++ project/windows10/launchApp.bat
@@ -19,14 +19,12 @@ echo Enter screen ID assigned in XLaunch (i.e. 0.0), found by hovering over XLau
 set /p XLaunchScreenID=
 
 echo The entered XLaunch display is %XLaunchScreenID%
-echo.
 
 timeout 3
 
-
 echo.
 echo Next step is to obtain the IPv4 address needed for Docker to access the screen.
-echo Look for the "IPv4 Address" from "Ethernet adapter vEthernet"
+echo Look for the "IPv4 Address" from "Ethernet adapter vEthernet (WSL)"
 echo i.e. "111.11.111.1"
 echo after pressing any key.
 echo.
@@ -36,18 +34,20 @@ pause
 ipconfig
 echo.
 
-echo Enter the "IPv4 Address" from "Ethernet adapter vEthernet" (Default Switch)
+echo "Enter the "IPv4 Address" from "Ethernet adapter vEthernet (WSL)"
 echo i.e. "111.11.111.1"
-echo "can highlight the entry -> control+c to copy"
-echo then right click in the command line to paste or can also manually type.
+echo can highlight the entry, "control+c" to "copy"
+echo then right click in the command line to paste
+echo or can also manually type.
 echo.
 
 set /p IPv4=
 
+echo.
 echo The entered IPv4 address is %IPv4%
 echo.
 
-PAUSE
+:: PAUSE
 
 :: Need to cd to the project folder, using this batch file as the reference point
 
@@ -66,4 +66,3 @@ PAUSE
 
 docker run --rm -ti --env DISPLAY=%IPv4%:%XLaunchScreenID% -p 127.0.0.1:8000:8000 -v "%cd%":/home/fenics/shared -v "%cd%":/project -w / dlbuesen/mat_mcr:910_10may21 ./launchApp.sh
 
-:: PAUSE
