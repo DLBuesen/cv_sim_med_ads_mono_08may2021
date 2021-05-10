9d7e47c121479aff4664e5934dea6a64fcc24c79
DLBuesen <50619551+DLBuesen@users.noreply.github.com>
2021-05-10 10:23:00 +0200

    updated repository

--- project/windows10/launchApp.bat
+++ project/windows10/launchApp.bat
@@ -1,30 +1,53 @@
 @echo off
 
+echo.
 echo Docker desktop should be running and configured to run Linux containers.
+echo.
 echo Should be signed in to dockerhub account.
 
 timeout 3
 
 echo checking login status...
+echo.
 docker login
 
 timeout 3
 
 echo XLaunch should be running with "Disable access control" selected.
+echo.
+echo Enter screen ID assigned in XLaunch (i.e. 0.0), found by hovering over XLaunch icon
+set /p XLaunchScreenID=
+
+echo The entered XLaunch display is %XLaunchScreenID%
+echo.
+
+timeout 3
+
+
+echo.
 echo Next step is to obtain the IPv4 address needed for Docker to access the screen.
+echo Look for the "IPv4 Address" from "Ethernet adapter vEthernet"
+echo i.e. "111.11.111.1"
+echo after pressing any key.
+echo.
 
 pause
 
 ipconfig
+echo.
 
-echo Enter the "IPv4 Address" from "Ethernet adapter vEthernet"
+echo Enter the "IPv4 Address" from "Ethernet adapter vEthernet" (Default Switch)
 echo i.e. "111.11.111.1"
+echo "can highlight the entry -> control+c to copy"
+echo then right click in the command line to paste or can also manually type.
+echo.
 
 set /p IPv4=
 
 echo The entered IPv4 address is %IPv4%
+echo.
 
-:: PAUSE
+PAUSE
 
 :: Need to cd to the project folder, using this batch file as the reference point
 
@@ -41,27 +64,6 @@ echo The entered IPv4 address is %IPv4%
 
 :: Run the container
 
-:: docker run --rm -ti --env DISPLAY=%IPv4%:00 -p 127.0.0.1:8000:8000 -v "%cd%":/home/fenics/shared -v "%cd%":/project -w / dlbuesen/cv_sim_med_ads_mono_08may2021:layer05 bash
+docker run --rm -ti --env DISPLAY=%IPv4%:%XLaunchScreenID% -p 127.0.0.1:8000:8000 -v "%cd%":/home/fenics/shared -v "%cd%":/project -w / dlbuesen/mat_mcr:910_10may21 ./launchApp.sh
 
-docker run --rm -ti --env DISPLAY=%IPv4%:00 -p 127.0.0.1:8000:8000 -v "%cd%":/home/fenics/shared -v "%cd%":/project -w / dlbuesen/mat_mcr:910_10may21 ./launchApp.sh
-
-:: docker run --rm --privileged -it --env DISPLAY=%IPv4%:00 -v "%cd%":/project dlbuesen/cv_sim_med_ads_mono_08may2021:layer04 ./launchApp.sh
-
-:: docker run --rm --privileged -it --env DISPLAY=%IPv4%:00 -v "%cd%":/project dlbuesen/cv_sim_med_ads_mono_08may2021:layer04 bash
-
-:: docker run --rm --privileged -it --env DISPLAY=%IPv4%:00 -v "%cd%":/project -v "%cd%":/home/fenics/shared -w / dlbuesen/cv_sim_med_ads_mono_08may2021:layer04 bash
-
-:: cd /project/src
-
-:: ./run_standaloneApp.sh /usr/local/MATLAB/MATLAB_Runtime/v910
-
-:: docker run --rm --privileged -ti --env DISPLAY=%IPv4%:00 -p 127.0.0.1:8000:8000 -v "%cd%":/home/fenics/shared -v "%cd%":/project -w / dlbuesen/cv_sim_med_ads_mono_08may2021:layer04 ./launchApp.sh'
-
-:: docker run --rm -it --env DISPLAY=%IPv4%:00 -v "%cd%":/project -v "%cd%":/home/fenics/shared -w / dlbuesen/cv_sim_med_ads_mono_08may2021:layer04 bash
-
-:: --privileged
-:: cd /project/src
-
-:: ./run_standaloneApp.sh /usr/local/MATLAB/MATLAB_Runtime/v910
-
-PAUSE
+:: PAUSE
